d7581f5bb4f52efb2c71cc5f8eb552db4f491a97
Matheus Campos <matheus_501@hotmail.com>
2022-06-20 12:43:06 -0300

    exercicio for php

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+
+    <form method="get" action="tabuada.php">
+        <select name="num" >
+
+        <?php
+        for($c=1; $c<=10; $c++) {
+            echo "<option>$c</option>";
+
+    
+        }
+           ?>
+           </select>
+
+           <input type="submit" value="tabuada"/>
+        
+
+        
+
+
+
+
+
+
+
+
+
+        </select>
+    
+
+
+
+
+    </div>
+    
+</body>
+</html>
